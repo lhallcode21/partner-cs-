a481d3a6a5b0032e865090c662a2337000169b54
ahekster <91039334+ahekster@users.noreply.github.com>
2021-09-19 18:57:47 -0700

    Update hw2pr2.py

--- hw2pr2.py
+++ hw2pr2.py
@@ -1,18 +1,24 @@
 # CS5 Gold, hw2pr2
 # Filename: hw2pr2.py
-# Name: Leah Hall and antonia hekster 
+# Name:
 # Problem description: Sleepwalking student
+
 import sys
 sys.setrecursionlimit(50000)
-import random
-import time
-import sys
+import random  
+
 def rs():
     """rs chooses a random step and returns it.
        Note that a call to rs() requires parentheses.
        Arguments: none at all!
     """
     return random.choice([-1, 1])
+
+import random
+random.choice([-1, 1])
+
+import time
+
 def rwpos(start, nsteps):
     """ rwpos models a random walker that
         * starts at the int argument, start
@@ -27,6 +33,9 @@ def rwpos(start, nsteps):
     else:
         newpos = start + rs()  # take one step
         return rwpos(newpos, nsteps - 1)
+
+import time
+
 def rwsteps(start, low, high):
     """ rwsteps models a random walker that
         * starts at the int argument, start  
@@ -36,16 +45,13 @@ def rwsteps(start, low, high):
         rwsteps returns the number of steps the
         walker needed to reach the lower or upper bound
     """
-    print("low, high, start are", low, high, start)   # show all of the arguments, as numbers...
-    print("_"*(start-low) + 'S')     # this is the beginning of a "terminal-graphics" wandering...
+    print('|' + "_"*(start-low) + 'S' + "_"*(high-start)+ '|')
     time.sleep(0.05)
 
     if start <= low or start >= high:
-        print '|' + (start - low) * ' ' + 'T.T' + (hi - start) * ' ' + '|'
         return 0
 
     else:
         newpos = start + rs()   # take one step, from start to newpos!
-        print '|' + (start - low) * ' ' + 'T.T' + (hi - start) * ' ' + '|'
+        print('|' + '_'*(newpos-low) + 'S' + '_'*(high-newpos) + '|')
         return 1 + rwsteps(newpos, low, high)
-
